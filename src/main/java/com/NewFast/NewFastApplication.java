4735460ed8c6ad6c8e5b6f83c456343ebb35047e
Lilyppu <lily@ppu.co.id>
2024-10-25 11:38:54 +0700

    first commit

---
+++ src/main/java/com/NewFast/NewFastApplication.java
@@ -0,0 +1,13 @@
+package com.NewFast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewFastApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewFastApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/NewFast/NewFastApplicationTests.java
@@ -0,0 +1,13 @@
+package com.NewFast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewFastApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
